6b235565735946a2895bb9c6fb4ab63c3837a6f7
steven43533 <steven43533@gmail.com>
2021-11-07 01:46:37 -0400

    Bot is now online

--- src/bot.js
+++ src/bot.js
@@ -1,1 +1,6 @@
-require('config.json').config();
+require("dotenv").config();
+
+const { Client } = require('discord.js');
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"]});
+
+client.login(process.env.TIMEZONEBOTJS_BOT_TOKEN);
